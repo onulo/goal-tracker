87b536293d34a0425b03b95730c0108530cae945
Ondrej Barci <ondrej.barci@ferratum.com>
2020-01-19 18:02:33 +0100

    change cors

--- src/main/java/com/obit/goaltracker/controller/ControllerConstant.java
+++ src/main/java/com/obit/goaltracker/controller/ControllerConstant.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 public final class ControllerConstant {
 
     public static final String CORS_LOCAL_ALLOWED = "http://localhost:4200";
-    public static final String CORS_PROD_ALLOWED = "https://goal-tracker-fe.herokuapp.com";
+    public static final String CORS_PROD_ALLOWED = "https://goaltracker.obit.sk";
 
     public static final String API_V_1 = "/api/v1";
 }
