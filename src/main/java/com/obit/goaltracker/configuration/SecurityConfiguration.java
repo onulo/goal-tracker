97716197fa19bdc0d68a3483245b6a9429baaac0
Ondrej Barci <ondrej.barci@ferratum.com>
2020-01-19 16:03:28 +0100

    disable cors

--- src/main/java/com/obit/goaltracker/configuration/SecurityConfiguration.java
+++ src/main/java/com/obit/goaltracker/configuration/SecurityConfiguration.java
@@ -13,7 +13,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable();
+        http.cors().and().csrf().disable();
         http.authorizeRequests()
                 .anyRequest()
                 .authenticated()
